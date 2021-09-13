ac555d41e1d49d042e4bbb60ac72b878ad8a1f1a
Lime Fortytwo <melberik@gmail.com>
2021-09-13 13:28:37 +0200

    Tried web scrapping

--- Search_Fight/Program.cs
+++ Search_Fight/Program.cs
@@ -4,6 +4,10 @@ using System.Collections.Specialized;
 using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
+using CsvHelper;
+using HtmlAgilityPack;
+using System.IO;
+using System.Globalization;
 
 namespace Search_Fight
 {
@@ -41,8 +45,20 @@ namespace Search_Fight
             var winner = GetWinner(winners);
 
             Console.WriteLine($"Total Winner: {winner.SearchWord}");
+
+            //WebScrap("tiger");
             Console.ReadKey();
 
+        }
+
+        public static void WebScrap(string searchWord)
+        {
+            HtmlWeb web = new HtmlWeb(); 
+            HtmlDocument doc = web.Load($"https://www.bing.com/search?q={searchWord}");
+            var headerNames = doc.DocumentNode.SelectNodes("//span[@class='sb_count']") ?? doc.DocumentNode.SelectNodes("/ html[1] / body[1] / div[1] / div[1] / main[1] / ol[1] / li[1] / h1[1]");
+            //var headerNames = doc.DocumentNode.SelectNodes("//main");
+
+            foreach (var item in headerNames) { Console.WriteLine(item.InnerText); }
         }
 
         /// <summary>
